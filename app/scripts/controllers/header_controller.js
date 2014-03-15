995ee20c86148354a594e90800f29f189c19615f
TCotton <me@andywalpole.me>
2014-03-15 10:53:39 +0000

    added main navigation

---
+++ app/scripts/controllers/header_controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('portfolioApp').controller('HeaderCtrl', ['$scope','LINKS', function ($scope,LINKS) {
+
+  $scope.navigation = {
+    items: [
+      {
+        label: 'My Work',
+        href: LINKS.MY_WORK
+      },
+      {
+        label: 'Side Projects',
+        href: LINKS.MY_WORK
+      },
+      {
+        label: 'About Me',
+        href: LINKS.SIDE_PROJECTS
+      },
+      {
+        label: 'Contact Me',
+        href: LINKS.CONTACT_ME
+      },
+      {
+        label: 'Blog',
+        href: LINKS.BLOG
+      }
+    ]
+  };
+
+}]);
