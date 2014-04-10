47602d088f38ad6e87b87730bd3800526e700d5e
Andy Walpole <me@andywalpole.me>
2014-04-10 18:22:46 +0100

    work in progress - the add blog page

--- app/scripts/controllers/blog_details_controller.js
+++ app/scripts/controllers/blog_details_controller.js
@@ -17,6 +17,9 @@
     this.$log = $log;
     this.$scope.addBlogFormData = {};
     this.$scope.addBlogFormData = new BlogMongoDB();
+    this.$scope.blogDelete = new BlogMongoDB();
+    this.$scope.allBlgs = BlogMongoDB;
+    this.$scope.blogContent = null;
     this.$scope.addBlogFormSubmit = false;
     this.$scope.formSuccess = null;
 
@@ -41,26 +44,29 @@
     };
 
 
-    var trimString = function() {
+    var trimString = function(blogObject) {
 
       // trim white space off the start and end of the string values after successful form submission
-      for(var key in this.$scope.addBlogFormData) {
+      for(var key in blogObject) {
 
-        if(key.isPrototypeOf(this.$scope.addBlogFormData) && _.isString(this.$scope.addBlogFormData[key])) {
+        if(key.isPrototypeOf(blogObject) && _.isString(blogObject)) {
 
-          this.$scope.addBlogFormData[key] = this.$scope.addBlogFormData[key].toString();
+          blogObject[key] = blogObject[key].toString();
 
         }
       }
+
+      return blogObject;
+
     }.bind(this);
 
 
-    var createContentSnippet = function () {
+    var createContentSnippet = function (data) {
 
       // to create a codeSnippet cut down the content to around 130 characters without cutting a whole word in half
       var snippet, maxLength, trimmedString;
 
-      snippet = this.$scope.addBlogFormData.content.toString();
+      snippet = data.toString();
 
       // maximum number of characters to extract
       maxLength = 130;
@@ -74,12 +80,13 @@
       trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(' '))) + ' ...';
 
       //strip and HTML tags
-      this.$scope.addBlogFormData.contentSnippet = trimmedString.replace(/(<([^>]+)>)/ig,'').trim();
+      return trimmedString.replace(/(<([^>]+)>)/ig,'').trim();
 
     }.bind(this);
 
 
-    var addSEOFriendlyURL = function () {
+    var addSEOFriendlyURL = function (data) {
+      // create seo friendly url for blog article title
 
       var stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'am', 'among', 'amongst', 'amoungst', 'amount', 'an', 'and', 'another', 'any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere', 'are', 'around', 'as', 'at', 'back', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'below', 'beside', 'besides', 'between', 'beyond', 'bill', 'both', 'bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant', 'co', 'con', 'could', 'couldnt', 'cry', 'de', 'describe', 'detail', 'do', 'done', 'down', 'due', 'during', 'each', 'eg', 'eight', 'either', 'eleven', 'else', 'elsewhere', 'empty', 'enough', 'etc', 'even', 'ever', 'every', 'everyone', 'everything', 'everywhere', 'except', 'few', 'fifteen', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'for', 'former', 'formerly', 'forty', 'found', 'four', 'from', 'front', 'full', 'further', 'get', 'give', 'go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'however', 'hundred', 'ie', 'if', 'in', 'inc', 'indeed', 'interest', 'into', 'is', 'it', 'its', 'itself', 'keep', 'last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made', 'many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine', 'more', 'moreover', 'most', 'mostly', 'move', 'much', 'must', 'my', 'myself', 'name', 'namely', 'neither', 'never', 'nevertheless', 'next', 'nine', 'no', 'nobody', 'none', 'noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'part', 'per', 'perhaps', 'please', 'put', 'rather', 're', 'same', 'see', 'seem', 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 'should', 'show', 'side', 'since', 'sincere', 'six', 'sixty', 'so', 'some', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 'still', 'such', 'system', 'take', 'ten', 'than', 'that', 'the', 'their', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'thereupon', 'these', 'they', 'thickv', 'thin', 'third', 'this', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 'un', 'under', 'until', 'up', 'upon', 'us', 'very', 'via', 'was', 'we', 'well', 'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with', 'within', 'without', 'would', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', 'the'];
 
@@ -90,7 +97,7 @@
       var newTitle;
 
       // initially remove hypthens and the white space to their right
-      newTitle = this.$scope.addBlogFormData.title.replace(/\–\s/g, '').toLowerCase();
+      newTitle = data.replace(/\–\s/g, '').toLowerCase();
 
       x = 0;
       l = stopwords.length;
@@ -111,7 +118,7 @@
       // remove all non-alpha numeric characters
       newTitle = newTitle.replace(regexNonAlphaNum, '');
 
-      this.$scope.addBlogFormData.title = newTitle;
+      return newTitle;
 
     }.bind(this);
 
@@ -142,11 +149,11 @@
     // check to make sure the form is completely valid
     if (isValid) {
 
-      this.trimString();
+      this.$scope.addBlogFormData = this.trimString(this.$scope.addBlogFormData);
       this.addDate();
       this.addUniqueID();
-      this.addSEOFriendlyURL();
-      this.createContentSnippet();
+      this.$scope.addBlogFormData.url = this.addSEOFriendlyURL(this.$scope.addBlogFormData.title);
+      this.$scope.addBlogFormData.contentSnippet = this.createContentSnippet(this.$scope.addBlogFormData.content);
 
       // submit details to mongodDB
       var returnedPromise = this.$scope.addBlogFormData.$save(function () {
@@ -172,17 +179,51 @@
         }
 
         this.$scope.addBlogFormSubmit = false;
-        //window.scrollTo(0, 0)
-        window.location = window.location.href + '#main-content';
 
       }.bind(this));
     }
 
   };
 
-  BlogDetailsCtrl.prototype.getBlog = function () {
+  BlogDetailsCtrl.prototype.getBlogs  = function () {
+    // request all blogs from the blog document
 
+    // return all user details from the user document
+    var returnedPromise = this.$scope.allBlgs.all(null, function () {
+    }, function (value) {
 
+      this.$log('Failure: UserDetailsCtrl.listAllUsers()', value);
+
+    }.bind(this));
+
+    returnedPromise.then(function (value) {
+
+      this.$scope.blogContent = value;
+
+    }.bind(this));
+
+  };
+
+  BlogDetailsCtrl.prototype.deleteArticle = function (data, deletePost){
+
+    // default value of attribute if not defined
+    deletePost = (typeof optionalArg === 'undefined') ? false : deletePost;
+
+    this.$scope.displayPopup = true;
+
+    this.$scope.blogDelete = data;
+
+  };
+
+  BlogDetailsCtrl.prototype.editArticle = function (data){
+
+    console.log(data);
+
+  };
+
+  BlogDetailsCtrl.prototype.hidePopup = function (){
+
+    this.$scope.displayPopup = null;
 
   };
 
