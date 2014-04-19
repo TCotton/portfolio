072826cdf73317b7a8e2805adff8e99ab0a17065
Andy Walpole <me@andywalpole.me>
2014-04-19 19:31:50 +0100

    basic nodejs configuration

--- server.js
+++ server.js
@@ -5,34 +5,31 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var util = require('util');
+var zlib = require('zlib');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var compression = require('compression');
 var app = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port = 9000;
-
-// configuration =================
 
 mongoose.connect('mongodb://AndyW:revolution@novus.modulusmongo.net:27017/sOzes7an'); 	// connect to mongoDB database on modulus.io
 
-app.set('port', process.env.PORT || 3000);
-//app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 9000);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/app')); 		// set the static files location /public/img will be /img for users
+app.use(compression());
 app.use(morgan('dev')); 						// log every request to the console
 app.use(bodyParser()); 							// pull information from html in POST
 app.use(methodOverride()); 						// simulate DELETE and PUT
 
+app.use(express.static(path.join(__dirname, 'app'))); 		// set the static files location /public/img will be /img for users
 
-http.createServer(app).listen(app.get('port'), function() {
+var server = http.createServer(app);
 
-  console.log('Express listening on port ' + app.get('port'));
-
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-
-
 
