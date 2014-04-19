3b84623da6a1c4331b515f55080dac88c4e2d31f
Andy Walpole <me@andywalpole.me>
2014-04-19 21:50:25 +0100

    changed form submit from GET to POST

--- app/scripts/services/post_form_service.js
+++ app/scripts/services/post_form_service.js
@@ -8,23 +8,24 @@
 
   var app = angular.module('portfolioApp');
 
-  var PostFormService = function ($http, $q) {
+  var PostFormService = function ($http, $q, CONFIG) {
 
     this.$http = $http;
     this.$q = $q;
+    this.CONFIG = CONFIG;
 
   };
 
-  PostFormService.$inject = ['$http', '$q'];
+  PostFormService.$inject = ['$http', '$q', 'CONFIG'];
 
   PostFormService.prototype.submitForm = function (formData) {
 
     var deferred = this.$q.defer();
 
     var returnedMessage = this.$http({
-      url: 'phppages/form_completion.php',
-      method: 'GET',
-      params: formData,
+      url: this.CONFIG.CURRENT_DOMAIN + 'phppages/form_completion.php',
+      method: 'POST',
+      data: formData,
       headers : {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
