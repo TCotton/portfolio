8688ad4ed2c45cd061ddab92c38d4518e181b33f
Andy Walpole <me@andywalpole.me>
2014-04-20 03:44:26 +0100

    removed another unwanted mongodo remote REST API file

--- app/scripts/services/blog_mongodb_service.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by andywalpole on 10/04/2014.
- */
-
-'use strict';
-
-angular.module('portfolioApp').factory('BlogMongoDB', function ($mongolabResourceHttp) {
-  return $mongolabResourceHttp('blog');
-});
