8a6b224df9cc70cc83db857e8afd9cce294f14e9
Andy Walpole <me@andywalpole.me>
2014-04-20 03:43:42 +0100

    removed unwanted remote mongo REST API

--- app/scripts/services/user_mongodb_service.js
+++
@@ -1,9 +0,0 @@
-///**
-// * Created by awalpole on 09/04/2014.
-// */
-
-'use strict';
-
-angular.module('portfolioApp').factory('UsersMongoDB', function ($mongolabResourceHttp) {
-  return $mongolabResourceHttp('user');
-});
