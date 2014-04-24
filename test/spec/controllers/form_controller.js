4a2b60ff79b0d2b44142bef75c3b830487f0908a
Andy Walpole <me@andywalpole.me>
2014-04-24 11:16:59 +0100

    test for FormCtrl as AdminFormCtrl

--- test/spec/controllers/form_controller.js
+++ test/spec/controllers/form_controller.js
@@ -4,31 +4,52 @@ describe('Controller: FormCtrl', function () {
   // load the controller's module
   beforeEach(module('portfolioApp', 'testConstants'));
 
-  var FormCtrl;
+  var AdminFormCtrl;
   var scope;
   var $controller;
   var $rootScope;
   var MOCK_DATA;
+  var $httpBackend;
+  var formDataSuccess = {'success': 'true', 'message': 'Thank you for taking the time to fill out the form. I will contact you as soon as I can!'};
+  var formData = {successMessage: null, successMessageDisable: null, name: 'Andy Walpole', email: 'andy_walpole@btopenworld.com', message: 'A message here'};
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$controller_, _$rootScope_, _MOCK_DATA_) {
+  beforeEach(inject(function (_$controller_, _$rootScope_, _MOCK_DATA_, _$httpBackend_) {
     $controller = _$controller_;
     $rootScope = _$rootScope_;
     MOCK_DATA = _MOCK_DATA_;
+    $httpBackend = _$httpBackend_;
 
     scope = $rootScope.$new();
-    FormCtrl = $controller('FormCtrl', {
+    AdminFormCtrl = $controller('FormCtrl as AdminFormCtrl', {
       $scope: scope
     });
   }));
 
-  it('Checks that scope changes to true after data is passed to the submitContactForm method', function () {
+  iit('Checks that scope changes to true after data is passed to the submitContactForm method', function () {
 
-    scope.$apply(function(){
-      scope.submitContactForm(MOCK_DATA.formController);
+    scope.contact = {};
+    scope.contact.name = formData.name;
+    scope.contact.email = formData.email;
+    scope.contact.message = formData.message;
+    scope.contact.successMessage = formData.successMessage;
+    scope.contact.successMessageDisable = formData.successMessageDisable;
+
+    $httpBackend.expect('POST', '/api/sendmail').respond(200, formDataSuccess);
+
+    scope.$apply(function () {
+      AdminFormCtrl.submitContactForm(true);
     });
 
-    expect(scope.submitted).toBe(true);
+    $httpBackend.flush();
+
+    expect(scope.contact.name).toBe(null);
+    expect(scope.contact.email).toBe(null);
+    expect(scope.contact.message).toBe(null);
+    expect(scope.contact.successMessage).toContain(formDataSuccess.message);
+    expect(scope.contact.successMessageDisable).toContain(formDataSuccess.success);
+
+    expect(scope.submitted).toBe(false);
 
   });
 
