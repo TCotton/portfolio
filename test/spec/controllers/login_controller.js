06234cda0818a5d94a1699062986b7435b7ddcce
Andy Walpole <me@andywalpole.me>
2014-04-24 10:12:38 +0100

    basics of LoginCtrl test

--- test/spec/controllers/login_controller.js
+++ test/spec/controllers/login_controller.js
@@ -13,12 +13,15 @@ describe('Controller: LoginCtrl as AdminLogin', function () {
   var $controller;
   var $rootScope;
   var MOCK_DATA;
+  var $httpBackend;
+  var returnUsrData = {'data':'\'creTheSweraqeSW8CunastevUfrezefr\'','status':200,'config':{'method':'POST','transformRequest':[null],'transformResponse':[null],'url':'/api/user/find','data':{'name':'AndyW','password':'aPassword'},'headers':{'Content-Type':'application/x-www-form-urlencoded','Accept':'application/json, text/plain, */*'}}};
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$controller_, _$rootScope_, _MOCK_DATA_) {
+  beforeEach(inject(function (_$controller_, _$rootScope_, _MOCK_DATA_, _$httpBackend_) {
     $controller = _$controller_;
     $rootScope = _$rootScope_;
     MOCK_DATA = _MOCK_DATA_;
+    $httpBackend = _$httpBackend_;
 
     scope = $rootScope.$new();
     AdminLogin = $controller('LoginCtrl as AdminLogin', {
@@ -28,11 +31,15 @@ describe('Controller: LoginCtrl as AdminLogin', function () {
 
   it('Checks that scope changes to true after data is passed to the AdminLogin.submitLoginForm method', function () {
 
+    $httpBackend.expect('POST', '/api/user/find').respond(200, returnUsrData.data);
+
     scope.$apply(function(){
-      AdminLogin.submitLoginForm(MOCK_DATA.loginController);
+      AdminLogin.submitLoginForm(true);
     });
 
-    expect(scope.submitted).toBe(true);
+    $httpBackend.flush();
+
+    expect(scope.userid).toBe(returnUsrData.data);
 
   });
 
