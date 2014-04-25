43102894a18af6b835a2742ec400b717c503a1fe
Andy Walpole <me@andywalpole.me>
2014-04-25 03:04:51 +0100

    homepageStatsDirective

--- app/scripts/directives/homepage_stats_directive.js
+++ app/scripts/directives/homepage_stats_directive.js
@@ -4,11 +4,44 @@
 
 'use strict';
 
-angular.module('portfolioApp').directive('homepageStatsDirective', [function () {
+angular.module('portfolioApp').directive('homepageStatsDirective', ['STATS', '$window', function (STATS, $window) {
 
   return {
     restrict: 'A',
-    link: function() {
+    templateUrl: 'views/homepage_stats.html',
+
+    link: function (scope) {
+
+      /** Only display the data that fits the screen width
+       * Less than 979px = display three blocks of data
+       * More that 979px = display all the data
+       * If matchMedia isn't supported (IE9) then fallback to display all the data
+       * **/
+
+      var handleMediaMatch = function (mql) {
+
+        if (!mql.matches) {
+
+          scope.stats = STATS;
+
+        } else {
+
+          scope.stats = _.toArray(STATS).slice(0, 3);
+
+        }
+      };
+
+      if ($window.matchMedia) {
+
+        var mql = $window.matchMedia('screen and (max-width: 979px)');
+        mql.addListener(handleMediaMatch);
+        handleMediaMatch(mql);
+
+      } else {
+
+        scope.stats = STATS;
+
+      }
     }
   };
 
