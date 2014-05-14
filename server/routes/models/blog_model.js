1b871e823d8b18e61d6f8d0b2656393cbda44636
Andy Walpole <me@andywalpole.me>
2014-05-14 18:12:25 +0100

    new mongoose blog schema

--- server/routes/models/blog_model.js
+++ server/routes/models/blog_model.js
@@ -5,16 +5,67 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-module.exports = mongoose.model('Blog', {
-  title : String,
-  author: String,
-  category: String,
-  content: String,
-  contentSnippet: String,
-  publishedDate: String,
-  url: String,
-  uniqueId: String,
-  displayImage: String,
-  commentsOpen: Boolean
+/**
+ * Blog Schema
+ */
+var BlogSchema = new Schema({
+  title: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  author: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  category: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  content: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  contentSnippet: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  aside: {
+    type: String,
+    trim: true
+  },
+  publishedDate: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  url: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  uniqueId: {
+    type: String,
+    default: '',
+    trim: true,
+    unique: true,
+  },
+  displayImage: {
+    type: String,
+    trim: true
+  },
+  commentsOpen: {
+    type: Boolean,
+    default: false,
+    trim: true
+  }
+
 });
+
+module.exports = mongoose.model('Blog', BlogSchema);
