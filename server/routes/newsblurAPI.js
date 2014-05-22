3a10bd0608a17eefd87744d2a6e112e9db445dd1
TCotton <me@andywalpole.me>
2014-05-22 21:57:10 +0100

    added new css borders

--- server/routes/newsblurAPI.js
+++ server/routes/newsblurAPI.js
@@ -26,11 +26,11 @@ module.exports = function () {
   };
 
   var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    //console.log('STATUS: ' + res.statusCode);
+    //console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
+      //console.log('BODY: ' + chunk);
     });
   });
 
