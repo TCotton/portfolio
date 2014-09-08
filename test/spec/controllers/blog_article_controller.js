205849e503ecdebf28694468891320f854d91d21
Andy Walpole <me@andywalpole.me>
2014-09-08 03:49:37 +0100

    beginnings of footer controller test

--- test/spec/controllers/blog_article_controller.js
+++ test/spec/controllers/blog_article_controller.js
@@ -2,12 +2,6 @@
  * Created by awalpole on 06/09/2014.
  */
 
-
-// use MOCK_DATA of array of blog content
-// check that all scope and rootScope properties have the correct values
-
-// check that 404 works if the conditional statement is not met
-
 'use strict';
 describe('Controller: BlogArticleCtrl as BlogArticlePageCtrl', function () {
 
