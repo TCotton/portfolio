7d69e6a3e4e5d3be8ec54b26038bdc235c3161c8
Andy Walpole <andy.walpole@gmail.com>
2020-10-31 16:03:03 +0000

    new js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,16 +292,9 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         mangle: true,
-        compress: {
-          sequences: true,
-          dead_code: true,
-          conditionals: true,
-          booleans: true,
-          unused: true,
-          if_return: true,
-          join_vars: true,
-          drop_console: true
-        }
+        preserveComments: true,
+        beautify: true,
+        compress: false
       }
     },
     posthtml: {
